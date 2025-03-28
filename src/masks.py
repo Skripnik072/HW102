6e2f4e4b03421b3a81642a3c51d079644f3ae2f8
Виктор Скрипник <viktor_ds@mail.ru>
2025-03-28 19:31:26 +0800

    fixed test2 masks

--- src/masks.py
+++ src/masks.py
@@ -12,12 +12,21 @@ def get_mask_card_number(number_card: str) -> str:
         return str(number_card)[0:4] + " " + str(number_card)[4:6] + "** " + "****" + " " + str(number_card)[-4:]
 
 
-print(get_mask_card_number(7000792289606361))
+# print(get_mask_card_number(7000792289606361))
 
 
 def get_mask_account(bank_account: int) -> str:
     """Функция возвращает маску банковского счёта"""
-    return "**" + str(bank_account)[-4:]
+    if bank_account == "":
+        raise ValueError('Не введён номер счёта')
+    elif str(bank_account).isdigit() == False:
+        raise ValueError('Некорректный номер')
+    elif int(bank_account) <= 0:
+        raise ValueError('Неверный номер счёта')
+    elif len(str(bank_account)) != 20:
+        raise ValueError('Неверный номер счётф')
+    else:
+        return "**" + str(bank_account)[-4:]
 
 
-# print(get_mask_account(73654108430135874305))
+print(get_mask_account(73654108430135874305))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,6 @@
 import pytest
 from src.masks import get_mask_card_number
+from src.masks import get_mask_account
 
 assert get_mask_card_number(7000792289606361) == "7000 79** **** 6361"
 
@@ -21,3 +22,26 @@ def test_empty_number_card(fixture_card):
 def test_empty_number_card(fixture_card):
     with pytest.raises(ValueError):
         get_mask_card_number("7000роgj35648749")
+
+
+assert get_mask_account(73654108430135874305) == "**4305"
+
+
+@pytest.fixture
+def fixture_account():
+    return (73654108430135874305)
+
+
+def test_invalid_number_card(fixture_account):
+    with pytest.raises(ValueError):
+        get_mask_account(7000)
+
+
+def test_empty_number_card(fixture_account):
+    with pytest.raises(ValueError):
+        get_mask_account("")
+
+
+def test_empty_number_card(fixture_account):
+    with pytest.raises(ValueError):
+        get_mask_account("7000роgj3564dfgh8749")
