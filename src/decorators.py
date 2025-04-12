0b59f727710383c55f302e01fbc1fd4c0f991fb4
Виктор Скрипник <viktor_ds@mail.ru>
2025-04-12 09:18:12 +0800

    fixed test decorators

--- src/decorators.py
+++ src/decorators.py
@@ -2,73 +2,37 @@ from time import time
 from functools import wraps
 
 
-def log(filename):
-    '''Декоратор для вывода результатов в файл или в консоль'''
-    def decorator(func):
+def log(filename=''):
+    ''' Декоратор для регистрации названия функции, её результатов и ошибок'''
+    def my_decorators(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
+                start_time = time()
                 result = func(*args, **kwargs)
-                log_result = (f'{func.__name__} "ok" "Result" {result}\n')
+                end_time = time()
+                log_result = (f'{func.__name__} ok\n')
                 if filename:
                     with open(filename, 'a', encoding='utf-8') as names_file:
                         names_file.write(log_result)
-                        print(log_result)
-                        return result
+                elif not filename:
+                    print(log_result)
+                return result
             except Exception as e:
-                log_result = (f'{func.__name__} "error" {e} "Input:" {args}, {kwargs}')
+                log_result = (f' {func.__name__} error {e} Input: {args}, {kwargs}\n')
                 if filename:
                     with open(filename, 'a', encoding='utf-8') as names_file:
                         names_file.write(log_result)
-                        print(log_result)
-            raise Exception(f"Max retries exceeded")
+                else:
+                    print(log_result)
+            raise Exception(f"Ошибка в работе декоратора")
 
         return wrapper
-    return decorator
+    return my_decorators
 
 
-def my_decorators(func):
-    ''' Декоратор для регистрации названия функции, её результатов и ошибок'''
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time()
-        result = func(*args, **kwargs)
-        end_time = time()
-        return result
-    return wrapper
-
-
-#def log(filename):
-#    ''' Декоратор для регистрации названия функции, её результатов и ошибок'''
-#    def my_decorators(func):
-#        def wrapper(*args, **kwargs):
-#            start_time = time()
-#            result = func(*args, **kwargs)
-#            end_time = time()
-
-#            if result <= 0:
-#                log_result = (f' {func.__name__} "error" "Input:" {args}, {kwargs} "Result:" {result}')
-#                if filename:
-#                    with open(filename, 'w', encoding='utf-8') as names_file:
-#                        names_file.write(log_result)
-#                print(log_result)
-#                raise ValueError(log_result)
-#            log_result = (f'{func.__name__} "ok" "Result" {result}')
-
-#            if filename:
-#                with open(filename, 'w', encoding='utf-8') as names_file:
-#                    names_file.write(log_result)
-#            print(log_result)
-
-#            return result
-#        return wrapper
-#    return my_decorators
-
-
-@log(filename="mylog.txt")
-@my_decorators
+@log()
 def my_function(x, y):
     return x + y
 
 my_function(3, 1)
-
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,10 +1,9 @@
 import pytest
 from src.decorators import log, my_function
-from src.decorators import my_decorators
 
 
-def test_my_decorator():
-    @my_decorators
+def test_my_decorators():
+    @log()
     def my_function(x, y):
         return x + y
 
@@ -12,6 +11,21 @@ def test_my_decorator():
     assert result == 8
 
 
-def test_log():
-    with pytest.raises(Exception, match="Max retries exceeded"):
+def test_my_decorators_in_file():
+    @log(filename='mylog.txt')
+    def my_function(x, y):
+        return x + y
+
+    result = my_function(3, 4)
+    assert result == 7
+
+
+def test_log_error():
+    with pytest.raises(Exception, match="Ошибка в работе декоратора"):
         my_function()
+
+
+def test_log_output(capsys):
+    my_function(3, 1)
+    captured = capsys.readouterr()
+    assert captured.out == 'my_function ok\n\n'
