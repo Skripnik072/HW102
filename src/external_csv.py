3e14461ff03c965417590a5672bc907126f8d6c3
Виктор Скрипник <viktor_ds@mail.ru>
2025-04-25 12:50:42 +0800

    fixed cod hw131

---
+++ src/external_csv.py
@@ -0,0 +1,24 @@
+import csv
+import pandas as pd
+
+
+def get_external_csv(path: str) -> list[dict]:
+    '''Функция принимает файл CSV и возвращает список словарей'''
+    my_dict = {}
+    my_list = []
+    with open(path, encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        for row in reader:
+            my_dict = {"id": row['id'], "state": row["state"], "date": row["date"],
+            "amount": row["amount"], "currency_name": row["currency_name"],
+            "currency_code": row["currency_code"], "from": row["from"], "to": row["to"],
+            "description": row["description"]}
+            my_list.append(my_dict)
+        print(my_list)
+
+    return my_list
+
+
+# if __name__ == '__main__':
+#    list_new = get_external_csv("date\\transactions.csv")
+#    print(list_new)
---
+++ src/external_xls.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import openpyxl
+
+
+def get_external_xls(path: str) -> list[dict]:
+    '''Функция принимает файл Excel и возвращает список словарей'''
+    my_dict = {}
+    my_list = []
+    excel_data = pd.read_excel(path)
+
+    for index, row in excel_data.iterrows():
+        my_dict = {"id": row['id'], "state": row["state"], "date": row["date"],
+        "amount": row["amount"], "currency_name": row["currency_name"],
+        "currency_code": row["currency_code"], "from": row["from"], "to": row["to"],
+        "description": row["description"]}
+        my_list.append(my_dict)
+
+    return my_list
+
+
+# if __name__ == '__main__':
+#    list_new = get_external_xls("date\\transactions_excel.xlsx")
+#    print(list_new)
+
